4056984e987a8c0f980a0cb10326c6069b06ea1c
황동현 <aaabbb4202@naver.com>
2026-08-09 14:24:56 +0900

    [SWEA] 1232 사칙연산

---
+++ Java_algorithm_practice/src/swea/D4/N1232/Solution.java
@@ -0,0 +1,85 @@
+package swea.D4.N1232;
+import java.io.*;
+import java.util.*;
+public class Solution{   
+    
+    static Node[] tree;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+        StringBuilder sb =new StringBuilder();
+
+        for(int t=1; t<=10; t++){
+            int N = Integer.parseInt(br.readLine());
+            tree = new Node[N+1];
+            for(int i = 0; i<N; i++){                
+                StringTokenizer st = new StringTokenizer(br.readLine());
+
+                int idx = Integer.parseInt(st.nextToken());
+                String value = st.nextToken();
+
+                if(st.countTokens() == 0){
+                    int num = Integer.parseInt(value);
+                    tree[idx] = new Node(num);
+                }
+                else{
+                    int left = Integer.parseInt(st.nextToken());
+                    int right = Integer.parseInt(st.nextToken());
+
+                    tree[idx] = new Node(value.charAt(0),left, right);
+                }
+                
+                
+            }
+
+            double result = calculate(1);
+
+            sb.append("#")
+              .append(t)
+              .append(" ")
+              .append((int) result)
+              .append("\n");
+    
+        }
+        System.out.println(sb);
+    }
+    static double calculate(int idx){
+        Node node = tree[idx];
+        
+        if(node.isNumber){
+            return node.number;
+        }
+
+        double left = calculate(node.left);
+        double right = calculate(node.right);
+
+        switch(node.operator){
+            case '+': return left+right;
+            case '-': return left-right;
+            case '*': return left*right;
+            case '/': return left/right;
+        }
+
+        return 0;
+    }
+
+    static class Node{
+        boolean isNumber;
+        double number;
+        char operator;
+        int left;
+        int right;
+
+        Node(double number){
+            this.isNumber = true;
+            this.number = number;
+        }
+
+        Node(char operator, int left, int right){
+            this.isNumber = false;
+            this.operator = operator;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ Java_algorithm_practice/src/swea/D4/N5251/Solution.java
@@ -0,0 +1,97 @@
+package swea.D4.N5251;
+import java.io.*;
+import java.util.*;
+
+/*
+다익스트라를 떠올리는 조건
+간선 비용 모두 0이상
+음수 가중치가 없는 경우
+*/
+
+
+/*
+우선순위 큐 사용이유
+다익스트라는 아직 처리하지 않은 정점 중 시작점으로부터 거리가 가장 짧은 정점을 선택
+가장 작은 값을 빠르게 꺼내는 것이 가능하다.
+*/
+public class Solution{
+    static int[] distance;
+    static int INF = Integer.MAX_VALUE;
+    static List<List<Edge>> graph;
+
+    static class Edge{
+        int to;
+        int weight;
+
+        Edge(int to, int weight){
+            this.to = to;
+            this.weight = weight;
+        }
+    }
+
+    static class Node implements Comparable<Node>{
+        int vertex;
+        int distance;
+
+        Node(int vertex, int distance){
+            this.vertex = vertex;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Node other){
+            return Integer.compare(this.distance, other.distance);
+        }
+    }
+
+    static void dijkstra(int start){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        Arrays.fill(distance, INF);
+
+        distance[start] = 0;
+        pq.offer(new Node(start, 0));
+
+        while(!pq.isEmpty()){
+            Node current = pq.poll();
+            int currentVertex = current.vertex;
+            int currentDistance = current.distance;
+
+            if(currentDistance > distance[currentVertex]){
+                continue;
+            }
+
+            for(Edge edge: graph.get(currentVertex)){
+                int nextVertex = edge.to;
+                int newDistance = currentDistance + edge.weight;
+
+                if(newDistance < distance[nextVertex]){
+                    distance[nextVertex] = newDistance;
+                    pq.offer(new Node(nextVertex, newDistance));                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int t=1; t<=T; t++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+
+            for(int i =0; i<N; i++){
+                st = new StringTokenizer(br.readLine());
+                //Map을 그래프로 입력
+                for(int j =0; j<N; j++){
+                    int s = Integer.parseInt(st.nextToken());
+                    int e = Integer.parseInt(st.nextToken());
+                    int w = Integer.parseInt(st.nextToken());
+                    graph.get(s).add(new Edge(e,w));
+                }
+            }
+
+            dijkstra(0);
+
+        }
+    }
+}
