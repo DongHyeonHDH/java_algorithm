0d47a7c7d6013389f6cb8fbdcb62a93988acf223
DonghyeonHDH <aaabbb4202@naver.com>
2026-08-26 18:00:01 +0900

    [SWEA] N27022 하나 걸러 수확 최대 초안

--- java_project_STS/src/N27022/N27022.java
+++ java_project_STS/src/N27022/N27022.java
@@ -8,25 +8,32 @@ public class N27022 {
 	static int[] mem;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		for(int tc = 1; tc<=T; tc++) {
 			N = Integer.parseInt(br.readLine());
-			tree = new int[N];
-			mem = new int[N];
+			tree = new int[N+1];
+			mem = new int[N+1];
+			int half = N/2;
 			
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int i = 0; i< N; i++) {
+			for(int i = 1; i<= N; i++) {
 				tree[i] = Integer.parseInt(st.nextToken());
 			}
 			
+			for(int i= 1; i<= half; i++) {			
+				mem[i] = mem[i-1]+Math.max(tree[2*i-1], tree[2*i]); 
+				
+			}
 			
+			sb.append("#")
+			.append(tc)
+			.append(" ")
+			.append(mem[half])
+			.append("\n")
+			;
 		}
-	}
+		System.out.println(sb);
+	}	
 	
-	static int func(int idx) {
-		for(int i= 0; i< N; i++) {			
-			mem[i] = Math.max(mem[i-1]+ tree[i], ); 
-			
-		}
-	}
 }
